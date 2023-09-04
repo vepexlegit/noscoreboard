428fc408886f80a3cc676dc9695ae8cab591f6b1
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-04 17:41:01 +0400

    Create NoScoreBoardCommand.java

---
+++ src/main/java/de/vepexlegit/noscoreboarddebug/command/NoScoreBoardCommand.java
@@ -0,0 +1,60 @@
+package de.vepexlegit.noscoreboarddebug.command;
+
+import de.vepexlegit.noscoreboarddebug.NoScoreBoard;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import java.util.Collections;
+import java.util.List;
+
+public class NoScoreBoardCommand implements ICommand {
+    @Override
+    public String getCommandName() {
+        return "noscoreboard";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return null;
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        if (args.length != 1 || (!args[0].equalsIgnoreCase("off") && !args[0].equalsIgnoreCase("on"))) {
+            sender.addChatMessage(new ChatComponentText("Invalid Arguments."));
+            return;
+        }
+
+        NoScoreBoard.INSTANCE.setEnabled(args[0].equalsIgnoreCase("on"));
+
+        sender.addChatMessage(new ChatComponentText(String.format("NoScoreBoard has been %s.",
+                NoScoreBoard.INSTANCE.isEnabled() ? "enabled" : "disabled")));
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+}
